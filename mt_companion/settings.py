e2bb7e34f1832a572f54eee26bb239c25c0d5cc1
rafiks <rab@devsphoto.com>
2022-01-04 22:07:07 -0500

    remove secret

--- mt_companion/settings.py
+++ mt_companion/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+# insttaldde python-decouple because I accidentally shared the Secret Key to
+# github
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k&n=(e3va)nt+&x7nrzkqfz!ss7%4_n@ur6om00c(i3^=f7#gy'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
